3953b2d6f368457f7b3df937738fc89ccec479ee
Adler  Medrado <adler@adlermedrado.com.br>
2018-02-05 09:31:58 -0200

    Add apps to project

--- health/health/settings.py
+++ health/health/settings.py
@@ -39,8 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # 3rd part libraries
     'rest_framework',
+    'django_extensions',
+
+    # Project apps
+    'apps.core',
+
 ]
 
 MIDDLEWARE = [
